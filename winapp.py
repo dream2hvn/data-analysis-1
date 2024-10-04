6c581e1ac70a8a1e0b753b7b0dfa08106bc56ea3
dream2hvn <138209676+dream2hvn@users.noreply.github.com>
2024-10-04 09:09:58 +0800

    winapp.py

--- winapp.py
+++ winapp.py
@@ -11,7 +11,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 all_data = pd.read_csv('/content/drive/MyDrive/all_data.csv')
